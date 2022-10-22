d068a339bf863024284ef93df67d2e372e82ffd8
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-10-22 10:51:10 +0300

    add route to main dashboard

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserHomepage from "../homeuser/UserHomepage";
+import '../../App.css';
+import Login from '../main-dashboard/Login';
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<UserHomepage />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/UserHome" element={<UserHomepage />} />
+
       </Routes>
     </Router>
   );
