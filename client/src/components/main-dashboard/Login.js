fde05f0a3c5aa86a2b7f7f0a3f5c2580089f51d6
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-10-23 19:07:45 +0300

    Create component for login form

--- client/src/components/main-dashboard/Login.js
+++ client/src/components/main-dashboard/Login.js
@@ -1,35 +1,13 @@
 import React from 'react';
-import './login.css';
-
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
-
-// import Sonnet from '../../components/Sonnet';
-
-function Login() {
-  return(
-    <>
-      <div className = "right-side" >Dashboard Carousel</div>
-      <div className = "left-side">Login-Signup</div>
 
 
+function Login(){
 
-    <Tabs
-      defaultActiveKey="profile"
-      id="uncontrolled-tab-example"
-      className="mb-3"
-    >
-      <Tab eventKey="login" title="Login">
-        Me
-      </Tab>
-      <Tab eventKey="register" title="Register">
-        Ivy
-      </Tab>
-     
-    </Tabs>
-
-    </>
-  )
+    return(
+        <>
+        Login Form
+        </>
+    );
 }
 
 export default Login;
