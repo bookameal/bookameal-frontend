b8c14167b2823b908adc334731e55cd9e446a69a
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-10-24 01:11:30 +0300

    Create sample user data to test out login method

--- client/src/components/main-dashboard/Dashboard.js
+++ client/src/components/main-dashboard/Dashboard.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import Login from './Login';
+import React, { useState } from 'react';
+import LoginForm from './LoginForm';
 import Register from './Register';
 import './dashboard.css'; 
 
@@ -9,17 +9,51 @@ import Tabs from 'react-bootstrap/Tabs';
 
 
 function Dashboard() {
-  return(
-    
-      <div classname="hero-section">
+
+  const adminUser = {
+      username: 'admin',
+      email: "admin@admin.com",
+      password: "admin123"
+  }
+
+  const [user, setUser] = useState({name: "", email: ""});
+  const [error, setError] = useState("");
+
+  const login = details => {
+
+      if (details.email === adminUser.email && details.password === adminUser.password) {
+        setUser({
+          name: details.name,
+          email: details.email
+        })
+  } else {
+        console.log("Details do not match!")
+  }
+}
+
+  const logout = () => {
+      setUser({name: "", email: ""});
+  }
+
+  return (
+      <div className="hero-section">
         <div className = "left-side" >Dashboard Carousel</div>
-        <div className = "right-side"> Sign
+        <div className = "right-side">
           <Tabs
             defaultActiveKey="profile"
             id="uncontrolled-tab-example"
             className="mb-3">
             <Tab eventKey="login" title="Login">
-              <Login />
+              <div>
+                {(user.email !== "") ? (
+                  <div className="welcome">
+                    <h2>Welcome, <span>{user.name}</span></h2>
+                    <button onClick={logout}>Logout</button>
+                  </div>
+                ): (
+                  <LoginForm login={login} error={error}/>
+                )}
+              </div>
             </Tab>
             <Tab eventKey="register" title="Register">
               <Register />
@@ -29,7 +63,7 @@ function Dashboard() {
       </div>
 
     
-  )
+  );
 }
 
 export default Dashboard;
