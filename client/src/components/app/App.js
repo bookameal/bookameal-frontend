3030ca70774549f53b6874a9929bdd833f0cfd48
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-10-26 01:07:30 +0300

    Add new routes for Login & Register Components

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserHomepage from "../homeuser/UserHomepage";
 import '../../App.css';
 import Dashboard from '../main-dashboard/Dashboard';
+import LoginForm from '../main-dashboard/LoginForm';
+import Register from '../main-dashboard/Register';
+// import Form from './components/Credentials/Form';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -15,7 +18,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/UserHome" element={<UserHomepage />} />
-
+        <Route path='/login' element={<LoginForm />}/>
+        <Route path='/register' element={<Register />}/>
       </Routes>
     </Router>
   );
