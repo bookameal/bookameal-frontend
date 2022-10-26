20fe164fa305ce4fd2f00be7d170d202ba3f99d2
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-10-26 03:17:47 +0300

    Fix Typo

--- client/src/components/main-dashboard/Dashboard.js
+++ client/src/components/main-dashboard/Dashboard.js
@@ -15,7 +15,7 @@ function Dashboard() {
         </div>
         <div className = "right-side">
             <h1>The flavors of life are just a bite away!</h1>
-            <p>At Tasty Treats, we're not just about preparing delicious meals. We're' a piece of our client’s lives,
+            <p>At Tasty Treats, we're not just about preparing delicious meals. We're a piece of our clients' lives,
                     and the networks in which we serve &#128522;</p>
             <button type="button" className="getStarted">Get Started</button>
       </div>
