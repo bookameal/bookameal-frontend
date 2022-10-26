8739e414327aa208815a71330c165ac6411121ff
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-10-26 03:15:45 +0300

    Import Nav-bar

--- client/src/components/main-dashboard/Register.js
+++ client/src/components/main-dashboard/Register.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './login.css'
-
+import Navigation from './Navigation';
+import './login.css';
+import regimg from '../../assets/Register2.jpg';
 
 function Register(){
 
@@ -44,28 +45,33 @@ function Register(){
 
 
   return (
-    <form action="" onSubmit={handleSubmit}>
-      <div className="form-inner">
-        <h2>Register to get started</h2> 
-        <div className="form-group">
-          <label htmlFor="name">Username:</label>
-          <input type="text" name="username" id="username-field" className="input-field" placeholder="Username" onChange={handleChange} required></input>        
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input type="email" name="email" id="usermail-field"  className="input-field" placeholder="Email" onChange={handleChange} required></input> 
-         </div>
-         <div className="form-group">
-            <label htmlFor="password">Password:</label>
-            <input type="password" name="password" id="password-field"  className="input-field" placeholder="Set Password" onChange={handleChange} required></input> 
-         </div>
-         <div className="form-group">
-            <label htmlFor="confirm-password">Confirm Password:</label>            
-            <input type="password" name="confirm-password" id="password-field2" className="input-field" placeholder="Confirm Password" onChange={handleChange} required></input> 
-         </div>
-         <button type='submit' className='log' id='logIn'>Sign Up</button>
-      </div>
-    </form>
+    <div className="join">
+        <Navigation />
+            <form action="" onSubmit={handleSubmit}>
+              <div className="form-inner reg">
+                <h2>Register to get started</h2> 
+                <div className="form-group">
+                  <label htmlFor="name">Username:</label>
+                  <input type="text" name="username" id="username-field" className="input-field" placeholder="Username" onChange={handleChange} required></input>        
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email:</label>
+                  <input type="email" name="email" id="usermail-field"  className="input-field" placeholder="Email" onChange={handleChange} required></input> 
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" name="password" id="password-field"  className="input-field" placeholder="Set Password" onChange={handleChange} required></input> 
+                </div>
+                <div className="form-group">
+                    <label htmlFor="confirm-password">Confirm Password:</label>            
+                    <input type="password" name="confirm-password" id="password-field2" className="input-field" placeholder="Confirm Password" onChange={handleChange} required></input> 
+                </div>
+                <button type='submit' className='log' id='logIn'>Sign Up</button>
+              </div>
+              <img src={regimg} alt="ramen" className="regimg" width ="770" height="760"/>
+
+            </form>
+    </div>
   );
 }
 
